7ddd86fea9fa45c0c83f2608d31a91c792379fbd
Marianna Ramos <marirsas.pt@gmail.com>
2025-02-14 16:09:32 +0000

    [Feat] Added angular components

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Components/header/header.component';
+import { ButtonComponent } from './Components/button/button.component';
+import { AdvertisementCardComponent } from './Components/advertisement-card/advertisement-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ButtonComponent,
+    AdvertisementCardComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Angular';

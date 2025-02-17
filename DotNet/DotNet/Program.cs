a665a1da5bfea058820800ecd5b4a46b59c80148
Iolanda Dourado <iolanda-dourado@hotmail.com>
2025-02-17 19:38:16 +0000

    Revert "[Merge] Merge feature/us02 with main."
    This reverts commit 631dca1f0913978a26e017af15215b061c22c1f3, reversing
    changes made to 274c97069d43cb7b44c881b8d669f151955d8cdb.

--- DotNet/DotNet/Program.cs
+++ DotNet/DotNet/Program.cs
@@ -1,62 +1,11 @@
-using DotNet.Data;
-using DotNet.Models;
-using DotNet.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<AccountService>();
-builder.Services.AddScoped<UserService>();
-builder.Services.AddScoped<AdminService>();
-
-builder.Services.AddDbContext<UserContext>(options => options.UseSqlServer
-    (builder.Configuration.GetConnectionString("UserDB"))
-    );
-
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<UserContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("UserDB")));
-
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => {
-    options.Password.RequiredLength = 8;
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-})
-    .AddEntityFrameworkStores<UserContext>()
-    .AddDefaultTokenProviders();
-
-
-builder.Services.AddAuthentication(options => {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(options => {
-        options.TokenValidationParameters = new TokenValidationParameters {
-            ValidateIssuer = true,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
-        };
-    });
-
-builder.Services.AddAuthorization(Options => {
-    Options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
-    Options.AddPolicy("UserPolicy", policy => policy.RequireRole("ApplicationUser"));
-});
 
 var app = builder.Build();
 
@@ -68,8 +17,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthentication();
 
 app.UseAuthorization();
 

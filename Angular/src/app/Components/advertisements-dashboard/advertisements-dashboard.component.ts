f199113bb97fe6efeb688dfbd228564725f6cce2
Marianna Ramos <marirsas.pt@gmail.com>
2025-02-21 16:14:17 +0000

    [Feat/Fix] Update dashboard

---
+++ Angular/src/app/Components/advertisements-dashboard/advertisements-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advertisements-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './advertisements-dashboard.component.html',
+  styleUrl: './advertisements-dashboard.component.css'
+})
+export class AdvertisementsDashboardComponent {
+
+}
--- Angular/src/app/Components/header/header.component.ts
+++ Angular/src/app/Components/header/header.component.ts
@@ -14,6 +14,7 @@ import { map } from 'rxjs';
 export class HeaderComponent implements OnInit {
   isDropdownOpen = false;
   isDropdown2Open = false;
+  isDropdown3Open = false;
   isLoggedIn$!: any;
   checkRole: string | null = null;
 
@@ -38,5 +39,13 @@ export class HeaderComponent implements OnInit {
 
   toggleDropdown2(isOpen: boolean) {
     this.isDropdown2Open = isOpen;
+  }
+
+  toggleDropdown3(isOpen: boolean) {
+    this.isDropdown3Open = isOpen;
+  }
+
+  toDash() {
+    this.myrouter.navigate(['dashboard']);
   }
 }

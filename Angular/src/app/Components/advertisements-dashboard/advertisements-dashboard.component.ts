9c7b7d916732a9938eff648080b4010d395b4d5b
DR-dev8 <diana8dev@gmail.com>
2025-02-22 01:07:12 +0000

    [Feat] Add view advertisements list in manager dashboard

--- Angular/src/app/Components/advertisements-dashboard/advertisements-dashboard.component.ts
+++ Angular/src/app/Components/advertisements-dashboard/advertisements-dashboard.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { AdvService } from '../../Services/adv.service';
+import { Adv } from '../../Models/adv';
+import { ButtonDeactivateAdvertisementComponent } from "../Buttons/button-deactivate-advertisement/button-deactivate-advertisement.component";
 
 @Component({
   selector: 'app-advertisements-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgxDatatableModule, ButtonDeactivateAdvertisementComponent],
   templateUrl: './advertisements-dashboard.component.html',
   styleUrl: './advertisements-dashboard.component.css'
 })
-export class AdvertisementsDashboardComponent {
+export class AdvertisementsDashboardComponent implements OnInit {
+  advertisements: Adv[] = [];
+  selectedAdv: any | null = null;
+  isModalOpen: boolean = false;
 
+  columns = [
+    { name: 'Title', prop: 'title', width: 100 },
+    { name: 'Status', prop: 'status', width: 50 }
+  ];
+  
+
+  constructor(private advService: AdvService) { }
+
+  ngOnInit(): void {
+    this.getAdvertisements();
+  }
+
+  getAdvertisements(): void {
+    this.advService.get('advertisements').subscribe({
+      next: (response) => {
+        this.advertisements = response;
+
+        console.log(this.advertisements)
+      },
+      error: (error) => {
+        console.error('Error fetching advs', error);
+      },
+    });
+  }
+
+  openModal(adv: Adv) {
+    this.selectedAdv = adv;
+    this.isModalOpen = true;
+  }
+  
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedAdv = null;
+  }
+
+  closeModalOut(event: Event) {
+    if (event.target === event.currentTarget) {
+      this.closeModal();
+    }
+  }
 }

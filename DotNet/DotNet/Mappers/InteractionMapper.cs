0388682afe46d3681d848bd1b56e4becc61265e7
Marianna Ramos <marirsas.pt@gmail.com>
2025-02-22 18:32:44 +0000

    [Feat] Create Tips and Interactions structure
    Create Model, DTO, Service Interface for Tip and Interaction Classes in DotNet project

---
+++ DotNet/DotNet/Mappers/InteractionMapper.cs
@@ -0,0 +1,26 @@
+﻿using DotNet.DTOs;
+using DotNet.Models;
+
+namespace DotNet.Mappers {
+    public static class InteractionMapper {
+        public static InteractionDTO ToDTO(Interaction interaction) {
+            return new InteractionDTO {
+                Id = interaction.Id,
+                TipId = interaction.TipId,
+                Like = interaction.Like,
+                Favorite = interaction.Favorite,
+                UserId = interaction.UserId
+            };
+        }
+
+        public static Interaction ToEntity(InteractionDTO interactionDTO) {
+            return new Interaction {
+                Id = interactionDTO.Id,
+                TipId = interactionDTO.TipId,
+                Like = interactionDTO.Like,
+                Favorite = interactionDTO.Favorite,
+                UserId = interactionDTO.UserId
+            };
+        }
+    }
+}
---
+++ DotNet/DotNet/Services/Interfaces/ITipsService.cs
@@ -0,0 +1,17 @@
+﻿using DotNet.DTOs;
+
+namespace DotNet.Services.Interfaces {
+    public interface ITipsService {
+        IEnumerable<TipDTO> GetTips();
+
+        Task<IEnumerable<TipDTO>> GetFavoritedTipsAsync(string userId);
+
+        TipDTO GetTipById(int id);
+
+        TipDTO AddTip(TipDTO tipDTO);
+
+        TipDTO RemoveTip(int id);
+
+        TipDTO UpdateTip(int id, TipDTO tipDTO);
+    }
+}

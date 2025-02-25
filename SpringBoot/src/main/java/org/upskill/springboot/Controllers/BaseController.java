5b461bc1d41294c4915d2aeb95d9518bbc68edc2
DR-dev8 <diana8dev@gmail.com>
2025-02-25 10:04:03 +0000

    [Fix] Add missing documentation in methods and classes

--- SpringBoot/src/main/java/org/upskill/springboot/Controllers/BaseController.java
+++ SpringBoot/src/main/java/org/upskill/springboot/Controllers/BaseController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * This abstract class provides a common base for all controllers in the application,
  * setting the base URL for all API endpoints to "/api/v1".
  */
-
 @RequestMapping("/api/v1")
 @CrossOrigin("*")
 public abstract class BaseController {
